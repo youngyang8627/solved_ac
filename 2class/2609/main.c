b0e2725fca45672c65506cb7afac3ae850f792da
youngyang8627 <youngyang8627@gmail.com>
2022-11-21 18:25:21 +0900

    11st commit 2022-11-21 6:25 PM

---
+++ 2class/2609/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+	short a,b;
+	scanf("%hd %hd",&a,&b);
+	int smax,smin=0;
+	int min,max;
+	int i=1,j=0;
+	if(a>b){
+		max = a;
+		min = b;
+	}
+	else{
+		max = b;
+		min = a;
+	}
+	while(i<max){
+		if(a%i==0&&b%i==0){
+			smax=i;
+		}
+		i++;
+	}
+	i = 1;
+	j=1;
+	while(smin<=0){
+		while(max*i>=min*j){
+			if(max*i==min*j){
+				smin=max*i;
+				break;
+			}
+			j++;
+		}
+		j=1;
+		i++;
+	}
+	printf("%d\n%d",smax,smin);
+	return 0;
+}
--- 2class/2775/main.c
+++ 2class/2775/main.c
@@ -2,4 +2,8 @@
 
 int main()
 {
-
+	int a;
+	scanf("%d",&a);
+	int i=0,j=0;
+	int k[a],n[a];
+	
